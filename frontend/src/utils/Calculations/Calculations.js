959a81a3a2bce9dc9f9212593af9881d6f697418
Jack Hilscher <jhilscheriv@gmail.com>
2021-09-01 17:43:26 -0400

    hp calc test

--- frontend/src/utils/Calculations/Calculations.js
+++ frontend/src/utils/Calculations/Calculations.js
@@ -1,8 +1,9 @@
 import { natures } from "../data/natures";
 export const hp = obj => {
-  let { base, iv, ev, level } = hp;
+  let { base, iv, ev, level } = obj;
 
-  return (2 * base + iv + (ev / 4) * level) / 100 + level + 10;
+  let evCalc = parseInt(ev / 4);
+  return parseInt(((2 * base + iv + evCalc) * level) / 100) + level + 10;
 };
 
 export const statCalc = (stat, nature, obj) => {
@@ -14,7 +15,6 @@ export const statCalc = (stat, nature, obj) => {
     natureMod = 0.9;
   }
   const evCalc = parseInt(ev / 4);
-  console.log(2 * base + iv + evCalc);
   return parseInt(
     (parseInt(((2 * base + iv + evCalc) * level) / 100) + 5) * natureMod
   );
--- frontend/src/utils/Calculations/Calculations.test.js
+++ frontend/src/utils/Calculations/Calculations.test.js
@@ -1,6 +1,11 @@
-import { statCalc } from "./Calculations";
+import { statCalc, hp } from "./Calculations";
 
 test("calculates stat with nature", () => {
   let obj = { base: 130, iv: 12, ev: 190, level: 78 };
   expect(statCalc("attack", "adamant", obj)).toBe(278);
+});
+
+test("calculates hp", () => {
+  let obj = { base: 108, iv: 24, ev: 74, level: 78 };
+  expect(hp(obj)).toBe(289);
 });

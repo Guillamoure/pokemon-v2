662e399bbf09b735d2cd25a5330b4d8251ee1536
Jack Hilscher <jhilscheriv@gmail.com>
2021-09-07 17:58:10 -0400

    displays default image if no image is provided, added --verbose as default to all tests

--- frontend/src/pages/PokemonInfo/PokemonDetails.js
+++ frontend/src/pages/PokemonInfo/PokemonDetails.js
@@ -1,12 +1,27 @@
 import React from "react";
 
 const PokemonDetails = ({ pokemon }) => {
-  const { name, national_pokedex_num: num } = pokemon;
+  const {
+    name,
+    national_pokedex_num: num,
+    front_sprite,
+    back_sprite
+  } = pokemon;
+
+  const sprite = () => {
+    let src = front_sprite || back_sprite || "missingno.png";
+    let alt = name;
+    if (src === "missingno.png") {
+      alt = "missingno";
+    }
+    return <img src={src} alt={alt} />;
+  };
 
   return (
     <section>
       <h2>{name}</h2>
       <span>{num}</span>
+      <div>{sprite()}</div>
     </section>
   );
 };
--- frontend/src/pages/PokemonInfo/PokemonDetails.test.js
+++ frontend/src/pages/PokemonInfo/PokemonDetails.test.js
@@ -3,16 +3,20 @@ import { screen, render } from "@testing-library/react";
 import PokemonDetails from "./PokemonDetails";
 
 describe("Artwork", () => {
-  test.todo("displays official artwork if it exists");
-  test.todo("displays front sprite if official artwork is not present");
-  test.todo("displays back sprite if front sprite is not present");
-  test.todo("displays default artwork with no artwork present");
-  test.todo("can click on carousel to view a different image");
-  test.todo("carousel will not do anything if there is no other image");
-  test.todo("displays thumbnail image above larger artwork");
-  test.todo("displays default image with no thumbnail image");
-  test.todo("shiny toggle will update front and back sprite");
-  test.todo(
-    "shiny toggle will not do anything if there is no front or back sprite"
-  );
+  // test.todo("displays official artwork if it exists");
+  // test.todo("displays front sprite if official artwork is not present");
+  // test.todo("displays back sprite if front sprite is not present");
+  test("displays default artwork with no artwork present", () => {
+    render(<PokemonDetails pokemon={{}} />);
+    const img = screen.getByRole("img");
+    expect(img.alt).toBe("missingno");
+  });
+  // test.todo("can click on carousel to view a different image");
+  // test.todo("carousel will not do anything if there is no other image");
+  // test.todo("displays thumbnail image above larger artwork");
+  // test.todo("displays default image with no thumbnail image");
+  // test.todo("shiny toggle will update front and back sprite");
+  // test.todo(
+  //   "shiny toggle will not do anything if there is no front or back sprite"
+  // );
 });
